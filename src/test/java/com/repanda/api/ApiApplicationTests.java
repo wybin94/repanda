a979d845072fa7978b732bf69775d78524e06531
wybin94 <dnjsdudqls@gmail.com>
2023-06-11 23:51:17 +0900

    first commit

---
+++ src/test/java/com/repanda/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repanda.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
